503faef92c6251768ed8d0bcfaf7e44b7259e740
syoji-haruki <j_0@docomo.ne.jp>
2019-03-27 21:13:07 +0900

    routes_修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#show'
   resources :users
-  resources :items do
-    resources :comments, only: [:create]
-  end
+  resources :items
+  resources :comments
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
