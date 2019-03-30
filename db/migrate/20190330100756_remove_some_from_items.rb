f5b11df63ea8c6b2762baa8ff6fb8498f14f60f2
usualpn0221 <47936786+usualpn0221@users.noreply.github.com>
2019-03-30 21:12:48 +0900

    Delete 20190330100756_remove_some_from_items.rb

--- db/migrate/20190330100756_remove_some_from_items.rb
+++
@@ -1,8 +0,0 @@
-class RemoveSomeFromItems < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :items, :buyer, foreign_key: true
-    remove_column :items, :postage, :string
-    remove_column :items, :region, :string
-    remove_column :items, :shipping_date, :string
-  end
-end
