329f94b7a0eda8739f37f6ba6343a9d586fbc3c5
syoji-haruki <j_0@docomo.ne.jp>
2019-03-30 13:49:42 +0900

    commit1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
 
 
   def show
+    @item = Item.find(params[:id]).includes(:saler_id)
     @items = Item.new
     @comment = Comment.new
     @items.id = 1
