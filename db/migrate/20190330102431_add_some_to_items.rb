196aa6b78d77939c0c227c6f09c1322b74e083b5
usualpn0221 <usualpn@gmail.com>
2019-03-30 19:56:48 +0900

    修正１

--- db/migrate/20190330102431_add_some_to_items.rb
+++ db/migrate/20190330102431_add_some_to_items.rb
@@ -1,6 +1,6 @@
 class AddSomeToItems < ActiveRecord::Migration[5.0]
   def change
-    add_column :items, :state, :string
+    add_column :items, :state, :integer
     add_reference :items, :category, foreign_key: true
   end
 end
