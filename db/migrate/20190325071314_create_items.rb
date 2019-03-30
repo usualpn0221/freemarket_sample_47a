aa6c1f64d94344709581f7f4da5b96be0927c55e
usualpn0221 <usualpn@gmail.com>
2019-03-30 21:09:39 +0900

    修正4

--- db/migrate/20190325071314_create_items.rb
+++ db/migrate/20190325071314_create_items.rb
@@ -2,14 +2,12 @@ class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
       t.string :name,null: false, index: true
-      t.string :description,null: false
-      t.string :condition,null: false
-      t.string :postage,null: false
-      t.string :region,null: false
-      t.string :shipping_date,null: false
+      t.text :description,null: false
+      t.string :status,null: false
       t.string :price,null: false
       t.references :saler,null: false
-      t.references :buyer
+      t.integer :state,null: false
+      t.references :category,null: false
 
 
 
