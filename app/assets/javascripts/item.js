3427287b50903ae92050b2dcb9b8f6fcba81c173
usualpn0221 <usualpn@gmail.com>
2019-04-01 20:08:56 +0900

    商品出品画像プレビュー表示の作成

---
+++ app/assets/javascripts/item.js
@@ -0,0 +1,31 @@
+$(function(){
+  $fileField = $('#itemnew_file')
+
+  // 選択された画像を取得し表示
+  $($fileField).on('change', $fileField, function(e) {
+    filecount = document.querySelector('input[type=file]').files.length;
+
+      $preview = $("#itemnew_img_field");
+      $preview.empty();
+
+    for(var i = 0; i < filecount ; i = i + 1){
+      file = e.target.files[i]
+      reader = new FileReader(),
+
+
+      reader.onload = (function(file) {
+        return function(e) {
+
+          $preview.append($('<img>').attr({
+            src: e.target.result,
+            width: "100%",
+            class: "itempreview",
+            title: file.name
+          }));
+        };
+      })(file);
+      reader.readAsDataURL(file);
+    }
+  });
+});
+
