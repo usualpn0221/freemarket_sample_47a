2616ff6b2ffccb2cdfe7e287713941525e276b27
usualpn0221 <usualpn@gmail.com>
2019-04-01 20:11:46 +0900

    スキーマファイル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190330111123) do
+ActiveRecord::Schema.define(version: 20190401051827) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
