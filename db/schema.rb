a79b27ea8884c54d7315e9cf7128601ab7c4eb3e
syoji-haruki <j_0@docomo.ne.jp>
2019-04-01 19:55:35 +0900

    Fix-comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190330111123) do
+ActiveRecord::Schema.define(version: 20190401051827) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20190330111123) do
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-
     t.string   "name",                                   null: false
     t.text     "description", limit: 65535,              null: false
     t.integer  "status",                    default: 1,  null: false
@@ -55,7 +54,6 @@ ActiveRecord::Schema.define(version: 20190330111123) do
     t.string   "saizu",                     default: "", null: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-
     t.index ["brand_id"], name: "index_items_on_brand_id", using: :btree
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
     t.index ["name"], name: "index_items_on_name", using: :btree
