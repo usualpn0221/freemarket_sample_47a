bfe63750e3d39301db9d005f559c5921241527af
orangina92 <user@USER-no-MacBook-Pro.local>
2019-04-02 20:17:29 +0900

    some-fix

--- db/migrate/20190402105310_rename_saler_id_column_to_user_id.rb
+++ db/migrate/20190402105310_rename_saler_id_column_to_user_id.rb
@@ -1,7 +1,7 @@
 class RenameSalerIdColumnToUserId < ActiveRecord::Migration[5.0]
   def change
     rename_column :items, :saler_id, :user_id
-    rename_column :items:, :state, :item_condition
-    rename_column :items:, :status, :trade_status
+    rename_column :items, :state, :item_condition
+    rename_column :items, :status, :trade_status
   end
 end
