8c3aead161ce642e9cfcb743b1322f7f220193f7
syoji-haruki <j_0@docomo.ne.jp>
2019-04-02 20:31:42 +0900

    編集

--- spec/ controllers/items_controller_spec.rb
+++ spec/ controllers/items_controller_spec.rb
@@ -1,14 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-describe ◯◯Controller do
-  describe 'HTTPメソッド名 #アクション名' do
-    it "インスタンス変数は期待した値になるか？" do
-　　"擬似的にリクエストを行ったことにするコードを書く"
-      "エクスペクテーションを書く"
-    end
+describe ItemsController do
+  # describe 'get #show' do
+  #   # it "インスタンス変数は期待した値になるか？" do
+  #   # "擬似的にリクエストを行ったことにするコードを書く"
+  #   #   "エクスペクテーションを書く"
+  #   # end
 
-    it "期待するビューに遷移するか？" do
-      "擬似的にリクエストを行ったことにするコードを書く"
-      "エクスペクテーションを書く"
-    end
-  end
+  #   # it "期待するビューに遷移するか？" do
+  #   #   "擬似的にリクエストを行ったことにするコードを書く"
+  #   #   "エクスペクテーションを書く"
+  #   # end
+  # end
+end
