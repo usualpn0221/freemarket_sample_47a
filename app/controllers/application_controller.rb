919a471d71f05d1cc661c19e1bece5e385dd3d4b
TakumiKATAOKA <kataokatakumi@kataokatakuhitsujinoMacBook-Air.local>
2019-04-02 16:30:34 +0900

    Add strongparameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:
+      [:nickname,
+      :firstname,
+      :lastname,
+      :firstnamekana,
+      :lastnamekana,
+      :birthdyear,
+      :birthmonth,
+      :birthday,
+      :postnumber,
+      :prefecture,
+      :shikuchouson,
+      :banchi,
+      :tatemonomei,
+      :phonenumber
+      ])
   def production?
     Rails.env.production?
   end
