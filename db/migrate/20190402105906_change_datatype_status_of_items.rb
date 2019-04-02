cfc87dc8459f6d8bbb0fd5720974f1d0a2373c16
orangina92 <user@USER-no-MacBook-Pro.local>
2019-04-02 20:25:46 +0900

    some-fix

--- db/migrate/20190402105906_change_datatype_status_of_items.rb
+++ db/migrate/20190402105906_change_datatype_status_of_items.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeStatusOfItems < ActiveRecord::Migration[5.0]
   def change
-    change_column :items, :status, :string
+    change_column :items, :trade_status, :string
   end
 end
