deeef406bb81ee0cfd8ea4d99e8f8e7e666fa57e
usualpn0221 <usualpn@gmail.com>
2019-04-03 09:43:24 +0900

    商品購入機能

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,10 +1,19 @@
 class BuysController < ApplicationController
   def new
+    @item=Item.find(itembuy_params[:item_id])
   end
 
   def create
+    @item=Item.find(itembuy_params[:item_id])
+    @item.trade_status = 3
+    @item.save
   end
 
   def index
+  end
+
+  private
+  def itembuy_params
+    params.permit(:item_id)
   end
 end
