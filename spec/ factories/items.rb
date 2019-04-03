3d00ecd855d87907842549427fa3ed3e28a52334
syoji-haruki <j_0@docomo.ne.jp>
2019-04-03 12:14:08 +0900

    編集

--- spec/ factories/items.rb
+++ spec/ factories/items.rb
@@ -3,13 +3,18 @@ FactoryBot.define do
 factory :item do
     name
     description
-    status
+    trade_status
     price
-    saler_
+    user_id
+    item_condition
+    category_id
+    brand_id
+    created_at
+    updated_at
 
-
-    text {"hello!"}
-    image {"hoge.png"}
+    # created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
+    # text {"hello!"}
+    # image {"hoge.png"}
     user
   end
 end
