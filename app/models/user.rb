2077bba6568028523424e02d18d72874a9b07d23
TakumiKATAOKA <kataokatakumi@kataokatakuhitsujinoMacBook-Air.local>
2019-04-05 15:00:17 +0900

    Edit  signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,30 @@ class User < ApplicationRecord
   has_many :comments
   has_many :uservaluations,foreign_key: "evaluateduser_id", class_name: "Uservaluation"
   has_one  :profile
+  # belongs_to :phonenumber, class_name: "profile"
+
+
+  def self.years
+   years = []
+   for year in 1900..2019 do
+     years << year
+   end
+   return years
+  end
+
+  def self.months
+   months = []
+   for month in 1..12 do
+     months << month
+   end
+   return months
+  end
+
+  def self.days
+   days = []
+   for day in 1..31 do
+     days << day
+   end
+   return days
+  end
 end
