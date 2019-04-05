7bf6f63a4aec00d273acffdb9e2eebbbc3da465c
TakumiKATAOKA <kataokatakumi@kataokatakuhitsujinoMacBook-Air.local>
2019-04-05 16:53:06 +0900

    Rails g controller phonenumber

---
+++ app/controllers/phonenumber_controller.rb
@@ -0,0 +1,7 @@
+class PhonenumberController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   get 'creditcards/index'
 
   get 'creditcards/edit'
@@ -20,6 +22,6 @@ Rails.application.routes.draw do
     end
     resources :comments
   end
-
+  resources :phonenumber
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
