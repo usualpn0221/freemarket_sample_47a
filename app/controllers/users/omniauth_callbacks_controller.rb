48260bc3e2edf7fdc2825270edb7c50dad9fede0
usualpn0221 <usualpn@gmail.com>
2019-04-13 21:04:19 +0900

    SNS認証

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,16 +15,36 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def passthru
   #   super
   # end
+  def facebook
+    callback_for(:facebook)
+  end
 
   # GET|POST /users/auth/twitter/callback
   # def failure
   #   super
   # end
+  def google_oauth2
+    callback_for(:google)
+  end
 
   # protected
+  def callback_for(provider)
+    @user = User.find_oauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #after_sign_in_path_forと同じパス
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_path
+    end
+  end
 
   # The path used when OmniAuth fails
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+  def failure
+    redirect_to root_path and return
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+         :recoverable, :rememberable, :validatable,
+         :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
+
   has_many :cards
   has_many :items
   has_many :trades
@@ -11,4 +14,35 @@ class User < ApplicationRecord
   has_many :uservaluations,foreign_key: "evaluateduser_id", class_name: "Uservaluation"
   has_one  :profile,dependent: :destroy
   has_one  :phonenumber,dependent: :destroy
+  has_many :snsCredentials, dependent: :destroy
+
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    if snscredential.present?
+      user = User.where(id: snscredential.user_id).first
+    else
+      user = User.where(email: auth.info.email).first
+      if user.present?
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+          )
+      else
+        user = User.create(
+          nickname: auth.info.name,
+          email:    auth.info.email,
+          password: Devise.friendly_token[0, 20],
+          )
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+          )
+      end
+    end
+    return user
+  end
 end
