b7bcd21484ea608995f91d5a42895a5ed2128b54
TakumiKATAOKA <kataokatakumi@kataokatakuhitsujinoMacBook-Air.local>
2019-04-18 15:28:09 +0900

    Edit application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,8 @@ class ApplicationController < ActionController::Base
 
   def delete_user
     @registrations=User.find(current_user.id)
-    @registrations.destroy if Profile.where(user_id: current_user.id).empty? || Phonenumber.where(user_id: current_user.id).empty? || @user_card =Card.where(user_id: current_user.id).empty?
+    binding.pry
+    @registrations.destroy unless Profile.where(user_id: current_user.id).exists? || Phonenumber.where(user_id: current_user.id).exists? || @user_card =Card.where(user_id: current_user.id).exists?
 
   end
 end
