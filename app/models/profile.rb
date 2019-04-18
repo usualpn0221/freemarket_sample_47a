03c35f752623bdbd12b37e2557847fde2d7dde88
TakumiKATAOKA <kataokatakumi@kataokatakuhitsujinoMacBook-Air.local>
2019-04-18 11:53:34 +0900

    edit profiles validates

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :lastname, presence: true,length: { maximum: 35 , allow_blank: true, message: "35文字以下で入力してください"}
-  validates :firstname, presence: true,length: { maximum: 35 ,message: "35文字以下で入力してください"}
-  validates :lastnamekana, presence: true,length: { maximum: 35, message: "35文字以下で入力してください"},format: { with:/\A[ア-ン]+\z/, message: "全角カタカナ以外は使用できません" }
-  validates :firstnamekana, presence: true,length: { maximum: 35 ,message: "35文字以下で入力してください"},format: { with:/\A[ア-ン]+\z/, message: "全角カタカナ以外は使用できません" }
+  validates :lastname,:firstname, presence: true,length: { maximum: 35 , allow_blank: true, message: "35文字以下で入力してください"}
+  validates :lastnamekana,:firstnamekana, presence: true,length: { maximum: 35, message: "35文字以下で入力してください"},format: { with:/\A[ア-ン]+\z/, message: "全角カタカナ以外は使用できません" }
   validates :postnumber, presence: true,format: { with:/\A[0-9]{3}-[0-9]{4}\z/, message: "フォーマットが不適切です"}
   validates :shikuchouson, presence: true,length: {maximum: 50 ,message: "50文字以下で入力してください"}
   validates :banchi, presence: true,length: {maximum: 100 ,message: "100文字以下で入力してください"}
