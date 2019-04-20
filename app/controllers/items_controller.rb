0e03a7242485d74ad969d05a44adb0c8fb76aa53
orangina92 <user@USER-no-MacBook-Pro.local>
2019-04-20 22:21:57 +0900

    some-fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,26 +5,14 @@ before_action :move_to_root,unless: :user_signed_in? ,only: :new
 
 
   def index
-    @search = Item.ransack(params[:q])
-    if @search.result
-      @ladies_items = @search.result.set_index(category_id:67..94).limit(4)
-      @mens_items = @search.result.set_index(category_id:95..120).limit(4)
-      @kids_items = @search.result.set_index(category_id:121..139).limit(4)
-      @cosme_items = @search.result.set_index(category_id:140..162).limit(4)
-      @channel_items = @search.result.set_index(brand_id:1).limit(4)
-      @louis_vuitton_items = @search.result.set_index(brand_id:3).limit(4)
-      @supreme_items = @search.result.set_index(brand_id:4).limit(4)
-      @nike_items = @search.result.set_index(brand_id:2).limit(4)
-    else
-      @ladies_items = Item.set_index(category_id:67..94).limit(4)
-      @mens_items = Item.set_index(category_id:95..120).limit(4)
-      @kids_items = Item.set_index(category_id:121..139).limit(4)
-      @cosme_items = Item.set_index(category_id:140..162).limit(4)
-      @channel_items = Item.set_index(brand_id:1).limit(4)
-      @louis_vuitton_items = Item.set_index(brand_id:3).limit(4)
-      @supreme_items = Item.set_index(brand_id:4).limit(4)
-      @nike_items = Item.set_index(brand_id:2).limit(4)
-    end
+    @ladies_items = @search.result.set_index(category_id:67..94).limit(4)
+    @mens_items = @search.result.set_index(category_id:95..120).limit(4)
+    @kids_items = @search.result.set_index(category_id:121..139).limit(4)
+    @cosme_items = @search.result.set_index(category_id:140..162).limit(4)
+    @channel_items = @search.result.set_index(brand_id:1).limit(4)
+    @louis_vuitton_items = @search.result.set_index(brand_id:3).limit(4)
+    @supreme_items = @search.result.set_index(brand_id:4).limit(4)
+    @nike_items = @search.result.set_index(brand_id:2).limit(4)
   end
 
   def show
