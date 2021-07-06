02bb45d96a57de1eadcfb2424d2208ffd5b4fcec
usualpn0221 <usualpn@gmail.com>
2021-07-06 23:41:27 +0900

    テスト３
    テスト３

---
+++ spec/factories/trades.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+
+  factory :trade do
+postage { 'neko' }
+delivery { 'neko' }
+region { 'neko' }
+shipping_date { 'neko' }
+  end
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,6 +4,13 @@ FactoryBot.define do
     sequence(:email) {Faker::Internet.email}
     password              { 'nekoneko' }
     password_confirmation { 'nekoneko' }
+    lastname              { 'ネコネコ' }
+    firstname              { 'ネコネコ' }
+    lastnamekana              { 'ネコ' }
+    firstnamekana              { 'ネコネコ' }
+    birthyear              { '2000' }
+    birthmonth              { '4' }
+    birthday              { '23' }
   end
 end
 
