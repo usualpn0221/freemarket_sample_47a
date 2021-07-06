c32569e592f71763e0227d0e8ce0ffe5c96fd974
usualpn0221 <usualpn@gmail.com>
2021-07-06 23:41:10 +0900

    コミット２
    テスト２

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,9 @@ RSpec.configure do |config|
 
 
   config.include FactoryBot::Syntax::Methods
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
 
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
